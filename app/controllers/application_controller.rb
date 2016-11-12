a7425d00dcd530c27d696d917b8818b81f9c2c9e
max pleaner <maxpleaner@gmail.com>
2016-11-11 20:13:31 -0800

    remove http auth, some bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  http_basic_authenticate_with(
-    username: ENV["HTTP_USERNAME"] || "bike",
-    password: ENV["HTTP_PASSWORD"] || "bike"
-  )
+  # http_basic_authenticate_with(
+  #   username: ENV["HTTP_USERNAME"] || "bike",
+  #   password: ENV["HTTP_PASSWORD"] || "bike"
+  # )
 
   def redirect_unpermitted
    redirect_to '/', notice: "can't let you do that ..."
